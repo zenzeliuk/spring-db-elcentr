7ef2ff4a1a5f7f6acf3aad405ae883f17add8cb5
maksym <zenzelyuk.m@gmail.com>
2021-03-22 01:04:24 +0200

    crud customer test

--- src/test/java/com/elcentr/dao/CustomerDAOTest.java
+++ src/test/java/com/elcentr/dao/CustomerDAOTest.java
@@ -5,13 +5,35 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CustomerDAOTest {
 
     @Autowired
     private CustomerDAO customerDAO;
+
+    @Test
+    void crudCustomer() {
+        Customer customer = Customer.builder()
+                .name("customer-test")
+                .notes("customer-notes")
+                .build();
+        Customer savedCustomer = customerDAO.save(customer);
+
+        assertNotNull(savedCustomer.getId());
+        assertEquals(savedCustomer, customerDAO.findById(savedCustomer.getId()).get());
+
+        List<Customer> customerList = customerDAO.findAll();
+
+        assertTrue(customerList.contains(savedCustomer));
+        customerDAO.delete(savedCustomer);
+        customerList = customerDAO.findAll();
+        assertFalse(customerList.contains(savedCustomer));
+
+    }
 
     @Test
     void createAndDeleteCustomer() {
